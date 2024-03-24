574d151b46d739dc94abecef29d06ca4a45a1f5a
EunHyoPark <parkeh0724@gmail.com>
2024-03-24 13:50:36 +0900

    입력값 처리 구현

--- 박은효/java-racingcar-6/src/main/java/racingcar/Application.java
+++ 박은효/java-racingcar-6/src/main/java/racingcar/Application.java
@@ -1,5 +1,11 @@
 package racingcar;
 
+import racingcar.Entity.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -11,6 +17,13 @@ public class Application {
         4. 시도 종료 후 우승자 결정(setWinner)
          */
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String names = scanner.nextLine();
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int t = scanner.nextInt(); //시도 횟수
 
 
     }
