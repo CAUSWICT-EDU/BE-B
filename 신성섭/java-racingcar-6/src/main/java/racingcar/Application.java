6bf8fae1326af50b23c007aed73a3cf68a7ae446
sinseongseop <sss02030@naver.com>
2024-03-24 17:09:56 +0900

    FEAT: Application 의존성 주입. 레이싱 게임 프로그램 완성

--- 신성섭/java-racingcar-6/src/main/java/racingcar/Application.java
+++ 신성섭/java-racingcar-6/src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.cardb.CarDbImp;
+import racingcar.cardb.RacingCarDb;
+import racingcar.racinggamemanager.GameManagerImp;
+import racingcar.racinggamemanager.RacingGameManager;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        GameManagerImp myRacingGame = new RacingGameManager();
+        CarDbImp myRacingCarDb = new RacingCarDb();
+        myRacingGame.setCarDb(myRacingCarDb);
+
+        myRacingGame.run();
+
     }
 }
