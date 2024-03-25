b0e6b09627731936b45df33baca8aeae5c04e6e1
Seown Min <nvpz1598@gamil.com>
2024-03-25 20:24:35 +0900

    #3 feat: Car 클래스 정의

--- 민세원/java-racingcar-6/src/main/java/racingcar/Car.java
+++ 민세원/java-racingcar-6/src/main/java/racingcar/Car.java
@@ -1,6 +1,20 @@
 package racingcar;
 
-public class Car {
-    // TODO: 멤버 변수: 사용자 이름
+class Car {
+    // TODO: 멤버 변수: 사용자 이름, 전진 횟수
+    String name;
+    int moveCount = 0;
+
+    //생성자
+    Car (String name) {
+        this.name = name;
+    }
+
     // TODO: 메서드: 전진
+    //
+    void move() {
+        this.moveCount += 1;
+    }
+
+
 }
