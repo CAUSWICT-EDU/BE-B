5fff9f06d85846b012c165436e999a30552d815e
Seown Min <nvpz1598@gamil.com>
2024-03-25 20:27:30 +0900

    #3 fix: 배열 -> ArrayList 속성 자료형 변경 및 반영

--- 민세원/java-racingcar-6/src/main/java/racingcar/Application.java
+++ 민세원/java-racingcar-6/src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
@@ -10,14 +11,20 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String [] carNameList = input.nextLine().split(",");
 
-//        for (String name: carNameList) {
-//            System.out.println(name.toString());
-//        }
+        ArrayList<Car> carArray = new ArrayList<>(); // Car 객체를 저장할 리스트
+
+        // Car 객체 생성
+        for (String name: carNameList) {
+            carArray.add(new Car(name));
+        }
 
         // TODO: 시도할 횟수 입력받기
         System.out.println("시도할 횟수는 몇회인가요?");
-        int attemptCount = input.nextInt();
+        int roundCount = input.nextInt();
 
+        // RacingGame 객체 생성
+        RacingGame myRacingGame = new RacingGame(carArray, roundCount);
+        myRacingGame.playGame();
 
     }
 
