fc1e9efbefbae8c29f6056376912eea1fb2cacc9
Seown Min <nvpz1598@gamil.com>
2024-03-25 18:55:28 +0900

    #3 feat: 자동차 이름 및 시도 횟수 입력

--- 민세원/java-racingcar-6/src/main/java/racingcar/Application.java
+++ 민세원/java-racingcar-6/src/main/java/racingcar/Application.java
@@ -1,11 +1,24 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         // TODO: 경주할 자동차 이름 입력받기
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String [] carNameList = input.nextLine().split(",");
+
+//        for (String name: carNameList) {
+//            System.out.println(name.toString());
+//        }
+
         // TODO: 시도할 횟수 입력받기
-        System.out.println("run");
-        System.out.println("실행");
+        System.out.println("시도할 횟수는 몇회인가요?");
+        int attemptCount = input.nextInt();
+
+
     }
 
 }
