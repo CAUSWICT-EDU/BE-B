ec7a6e3ee97671b697b86008f068472c6b6c048f
Seown Min <nvpz1598@gamil.com>
2024-03-25 20:25:28 +0900

    #3 feat: RacingGame 클래스 정의

--- 민세원/java-racingcar-6/src/main/java/racingcar/RacingGame.java
+++ 민세원/java-racingcar-6/src/main/java/racingcar/RacingGame.java
@@ -1,6 +1,81 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class RacingGame {
     // TODO: 멤버 변수: 참여자 명단, 우승자 명단, 시도 횟수
-    // TODO: 메서드: 회차당 결과 출력, 우승 결정, 회차당 랜덤 수 리턴
+    ArrayList<Car> carArray;
+    ArrayList<String> winnerList = new ArrayList<>();
+    int roundCount;
+
+    RacingGame(ArrayList<Car> cars, int roundCount) {
+        this.carArray = cars;
+        this.roundCount = roundCount;
+    }
+
+    // 랜덤 수 리턴 메서드
+    int getRandomNumber() {
+        return (int) (Math.random() * 10); // 0에서 9사이(10 미포함)
+    }
+
+    // 라운드 결과 출력 메서드
+    void printRoundResult() {
+        for (Car car : carArray) {
+            System.out.println(car.name + " : " + "-".repeat(car.moveCount));
+        }
+        System.out.println();
+    }
+
+    // 라운드 진행 메서드
+    void playRound() {
+        for (Car car : carArray) {
+            if (this.getRandomNumber() >= 4) {
+                car.move();
+            }
+        }
+    }
+
+    // 가장 먼 거리 찾는 메서드
+    int getMaxMoveCount() {
+        int max = carArray.get(0).moveCount;
+
+        for (int i = 1; i < carArray.size(); i++) {
+            int toCompare = carArray.get(i).moveCount;
+            if (max < toCompare) {
+                max = toCompare;
+            }
+        }
+
+        return max;
+    }
+
+    // 우승자 결정 메서드
+    void decideFinalWinner(int max) {
+        System.out.print("최종 우승자: ");
+
+        // winnerList 우승자들 추가
+        for (Car  car : carArray) {
+            if (car.moveCount == max) {
+                winnerList.add(car.name);
+            }
+        }
+        // 우승자 출력
+        String finalWinners = String.join(", ", winnerList);
+        System.out.println(finalWinners);
+    }
+
+
+    // 게임 진행
+    void playGame() {
+        System.out.println("실행 결과");
+
+        for (int i = 0; i < this.roundCount; i++) {
+            this.playRound();
+            this.printRoundResult();
+        }
+
+        int maxMoveCount = this.getMaxMoveCount();
+        this.decideFinalWinner(maxMoveCount);
+
+    }
 }
