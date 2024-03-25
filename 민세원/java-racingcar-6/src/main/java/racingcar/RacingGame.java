398cfafd2038d81c36d4a813f01d23eb986cc1cc
Seown Min <nvpz1598@gamil.com>
2024-03-25 23:24:24 +0900

    #3 fix: update 접근 제어자 protected -> private

--- 민세원/java-racingcar-6/src/main/java/racingcar/RacingGame.java
+++ 민세원/java-racingcar-6/src/main/java/racingcar/RacingGame.java
@@ -15,12 +15,12 @@ public class RacingGame {
     }
 
     // 랜덤 수 리턴 메서드
-    protected int getRandomNumber() {
+    private int getRandomNumber() {
         return (int) (Math.random() * 10); // 0에서 9사이(10 미포함)
     }
 
     // 라운드 결과 출력 메서드
-    protected void printRoundResult() {
+    private void printRoundResult() {
         for (Car car : carArray) {
             System.out.println(car.name + " : " + "-".repeat(car.moveCount));
         }
@@ -28,7 +28,7 @@ public class RacingGame {
     }
 
     // 라운드 진행 메서드
-    protected void playRound() {
+    private void playRound() {
         for (Car car : carArray) {
             if (this.getRandomNumber() >= 4) {
                 car.move();
@@ -37,7 +37,7 @@ public class RacingGame {
     }
 
     // 가장 먼 거리 찾는 메서드
-    protected int getMaxMoveCount() {
+    private int getMaxMoveCount() {
         int max = carArray.get(0).moveCount;
 
         for (int i = 1; i < carArray.size(); i++) {
@@ -51,7 +51,7 @@ public class RacingGame {
     }
 
     // 우승자 결정 메서드
-    protected void decideFinalWinner(int max) {
+    private void decideFinalWinner(int max) {
         // winnerList 우승자들 추가
         for (Car  car : carArray) {
             if (car.moveCount == max) {
@@ -61,7 +61,7 @@ public class RacingGame {
     }
 
     // 우승자 출력 메서드
-    protected void printFinalResult() {
+    private void printFinalResult() {
         String finalWinners = String.join(", ", winnerList);
         System.out.println(finalWinners);
     }
@@ -69,7 +69,7 @@ public class RacingGame {
 
 
     // 게임 진행 메서드
-    protected void playGame() {
+    public void playGame() {
         System.out.println("실행 결과");
 
         for (int i = 0; i < this.roundCount; i++) {
