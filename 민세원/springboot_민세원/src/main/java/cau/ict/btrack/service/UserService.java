47e86291d4007edbd7f7e0503b4e7feaf3563315
givemethatsewon <nvpz1598@cau.ac.kr>
2024-05-13 19:58:09 +0900

    #14 feat: getSimpleUserInfo 작성

--- 민세원/springboot_민세원/src/main/java/cau/ict/btrack/service/UserService.java
+++ 민세원/springboot_민세원/src/main/java/cau/ict/btrack/service/UserService.java
@@ -1,14 +1,30 @@
 package cau.ict.btrack.service;
 
+import cau.ict.btrack.domain.User;
+import cau.ict.btrack.dto.ResponseSimpleUserDto;
+import cau.ict.btrack.exception.BaseException;
 import cau.ict.btrack.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import cau.ict.btrack.util.ResponseCode;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public ResponseSimpleUserDto getSimpleUserInfo(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            return new ResponseSimpleUserDto(user.getName(), user.getHeight(), user.getAge());
+        } else {
+            throw new BaseException(ResponseCode.USER_NOT_FOUND);
+        }
     }
 }
