f6facb124a0addf10c80e9aba74774de0e9c0866
sinseongseop <sss02030@naver.com>
2024-05-18 12:43:50 +0900

    Feat : BaseEntity 설정

--- 신성섭/springboot_seongseop/src/main/java/com/ict/btrack/BtrackApplication.java
+++ 신성섭/springboot_seongseop/src/main/java/com/ict/btrack/BtrackApplication.java
@@ -2,8 +2,10 @@ package com.ict.btrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class BtrackApplication {
 
 	public static void main(String[] args) {
---
+++ 신성섭/springboot_seongseop/src/main/java/com/ict/btrack/domain/common/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.ict.btrack.domain.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
