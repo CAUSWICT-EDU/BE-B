7e2059e693d9a3ea9997b679a1cc8005d742dc9c
OH-GITAEK <dhrlxor1512@gmail.com>
2024-05-22 16:54:57 +0900

    [feat] create Enum package && javafiles

---
+++ 오기택/springboot_오기택/src/main/java/cau/ict/btrack/domain/enums/Like.java
@@ -0,0 +1,5 @@
+package cau.ict.btrack.domain.enums;
+
+public enum Like {
+    LIKE,UNLIKE;
+}
---
+++ 오기택/springboot_오기택/src/main/java/cau/ict/btrack/domain/enums/MemberStatus.java
@@ -0,0 +1,5 @@
+package cau.ict.btrack.domain.enums;
+
+public enum MemberStatus {
+    ACTIVE,INACTIVE
+}
