891beace10c22411311389b60aaae8308940d7d6
givemethatsewon <nvpz1598@cau.ac.kr>
2024-05-23 17:15:57 +0900

    #31 fix: 자잘한 오류 해결

--- 민세원/springboot_민세원/src/main/java/cau/ict/btrack/domain/LikeMap.java
+++ 민세원/springboot_민세원/src/main/java/cau/ict/btrack/domain/LikeMap.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Like {
+public class LikeMap {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
