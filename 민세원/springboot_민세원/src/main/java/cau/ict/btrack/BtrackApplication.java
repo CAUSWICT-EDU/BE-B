d0b99437fef09c3fc075bb295651d7c55db3e78e
givemethatsewon <nvpz1598@cau.ac.kr>
2024-05-23 12:25:46 +0900

    #31 feat: BaseEntity 설정

--- 민세원/springboot_민세원/src/main/java/cau/ict/btrack/BtrackApplication.java
+++ 민세원/springboot_민세원/src/main/java/cau/ict/btrack/BtrackApplication.java
@@ -2,8 +2,10 @@ package cau.ict.btrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BtrackApplication {
 
 	public static void main(String[] args) {
