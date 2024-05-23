48263d759cf9a9548f6883c2965b923c0c4ff216
givemethatsewon <nvpz1598@cau.ac.kr>
2024-05-23 12:43:59 +0900

    #31 feat: Member 엔티티 작성

---
+++ 민세원/springboot_민세원/src/main/java/cau/ict/btrack/domain/Member.java
@@ -0,0 +1,32 @@
+package cau.ict.btrack.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String nickName;
+
+    private String email;
+
+    private String password;
+
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+}
