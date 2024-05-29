9e683655f7a8724618c3e4fbfc6413d3910a35ae
givemethatsewon <nvpz1598@cau.ac.kr>
2024-05-29 19:19:30 +0900

    DB 등 프로젝트 초기설정

---
+++ 민세원/boardCRUD/src/test/java/org/example/boardcrud/BoardCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.boardcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
