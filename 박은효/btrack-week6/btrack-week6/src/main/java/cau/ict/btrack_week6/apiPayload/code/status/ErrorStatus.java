d8874ad43674c1534ec99f26cdecd4e32ca63840
acidify04 <parkeh0724@gmail.com>
2024-05-29 17:59:21 +0900

    에러 코드 작성

--- 박은효/btrack-week6/btrack-week6/src/main/java/cau/ict/btrack_week6/apiPayload/code/status/ErrorStatus.java
+++ 박은효/btrack-week6/btrack-week6/src/main/java/cau/ict/btrack_week6/apiPayload/code/status/ErrorStatus.java
@@ -13,16 +13,18 @@ public enum ErrorStatus implements BaseErrorCode {
     // 가장 일반적인 응답
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
     _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON400","잘못된 요청입니다."),
-    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","인증이 필요합니다."),
+    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","로그인이 필요합니다."),
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
 
-    // 멤버 관려 에러
+    // 멤버 관련 에러
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "사용자가 없습니다."),
-    NICKNAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "닉네임은 필수 입니다."),
+    NAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "이름은 필수 입니다."),
+    PASSWORD_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4003", "비밀번호는 필수 입니다."),
 
-    // 예시,,,
-    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다.");
+    // 게시글, 댓글 관련 에러
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "POST4001", "게시글이 없습니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "POST4002", "댓글이 없습니다.");
 
 
     private final HttpStatus httpStatus;
