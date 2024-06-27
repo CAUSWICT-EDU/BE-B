da92e423e7bb8805a3f428ce613e4f7845f1f73d
sinseongseop <sss02030@naver.com>
2024-06-28 00:26:20 +0900

    FIX: 잘못된 genericType 선언 수정.

--- 신성섭/community/src/main/java/dongne/community/domain/post/domain/repository/PostRepository.java
+++ 신성섭/community/src/main/java/dongne/community/domain/post/domain/repository/PostRepository.java
@@ -1,8 +1,9 @@
 package dongne.community.domain.post.domain.repository;
 
 import dongne.community.domain.member.domain.Member;
+import dongne.community.domain.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository  extends JpaRepository<Member, Long> {
+public interface PostRepository  extends JpaRepository<Post, Long> {
 
 }
