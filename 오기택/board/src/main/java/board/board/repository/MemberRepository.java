e7d9f86714f8a6794c87fb81ecc16f21ada3ef46
OH-GITAEK <dhrlxor1512@gmail.com>
2024-06-28 12:06:47 +0900

    [feat] create repository

---
+++ 오기택/board/src/main/java/board/board/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package board.board.repository;
+
+import board.board.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
---
+++ 오기택/board/src/main/java/board/board/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package board.board.repository;
+
+import board.board.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
