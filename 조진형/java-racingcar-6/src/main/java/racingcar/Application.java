2859148f8bbb0d3de843c632deeca21483939b9a
ErrCode0987 <44331678+macqueen0987@users.noreply.github.com>
2025-03-27 17:46:48 +0900

    과제1 초안
    인텔리제이에서 푸시가 안되서 수동으로 올려봅니다

--- 조진형/java-racingcar-6/src/main/java/racingcar/Application.java
+++ 조진형/java-racingcar-6/src/main/java/racingcar/Application.java
@@ -1,7 +1,53 @@
 package racingcar;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        // 자동차 이름 입력
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = sc.nextLine();
+        racingcar [] cars = new racingcar[3];
+        if (carNames.split(",").length != 3) { // 자동차 이름은 3개여야 함
+            throw new IllegalArgumentException("자동차 이름은 3개를 입력해야 합니다.");
+        }
+        String[] carName = carNames.split(",");
+        for (int i = 0; i < carName.length; i++) {
+            // 자동차 객체 생성
+            cars[i] = new racingcar(carName[i].strip());
+        }
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryCount;
+        try {
+            tryCount = sc.nextInt();
+        }
+        catch (InputMismatchException exception) {
+            throw new IllegalArgumentException("시도 횟수는 정수여야 합니다.");
+        }
+        if (tryCount < 1) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+        // 시도 횟수만큼 자동차 이동
+        System.out.println("실행 결과:");
+        for (int i = 0; i < tryCount; i++) {
+            for (racingcar car : cars) {
+                car.race();
+            }
+            System.out.println();
+        }
+
+        // 우승자 출력
+        int maxPosition = 0;
+        for (racingcar car : cars) {
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+        System.out.print("최종 우승자: ");
+        for (racingcar car : cars) {
+            if (car.getPosition() == maxPosition) {
+                System.out.print(car.carname + " ");
+            }
+        }
     }
 }
