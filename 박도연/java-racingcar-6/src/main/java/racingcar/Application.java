6dc8d140e5bcd80d8b7747e054f2bfb710bb9bf5
Doyeon <doyeon71013048@gmail.com>
2025-04-01 01:04:12 +0900

    feat : exception for tryCount

--- 박도연/java-racingcar-6/src/main/java/racingcar/Application.java
+++ 박도연/java-racingcar-6/src/main/java/racingcar/Application.java
@@ -2,7 +2,29 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private static int checkTryCountValidate(String sTryCount) {
+        int tryCount;
+
+        // 1. 빈 입력값 예외 처리
+        if (sTryCount == null || sTryCount.trim().isEmpty()) {
+            throw new IllegalArgumentException("시도 횟수는 비어 있을 수 없습니다.");
+        }
+        try {
+            tryCount = Integer.parseInt(sTryCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 양의 정수여야 합니다.");
+        }
+
+        // 3. 양수인지 검증
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 양의 정수여야 합니다.");
+        }
+
+        return tryCount;
+    }
+
     public static void main(String[] args) {
+
         // initialize - 1 자동차 목록 입력
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
@@ -12,10 +34,8 @@ public class Application {
         System.out.println("시도할 회수는 몇회인가요?");
         String sTryCount = Console.readLine();
 
-        // TODO: tryCount에 대한 exception 구현
-        int iTryCount = Integer.parseInt(sTryCount);
+        int iTryCount = checkTryCountValidate(sTryCount);
 
-        // TODO: Car 객체 구성할 때 name과 trycount에 대한 exception 구현
         // initalize - 3 자동차 객체 생성
         Car[] cars = new Car[carNames.length];
         for (int i = 0; i < carNames.length; i++) {
@@ -51,4 +71,6 @@ public class Application {
         }
         System.out.println("최종 우승자 : " + winner);
     }
+
+
 }
