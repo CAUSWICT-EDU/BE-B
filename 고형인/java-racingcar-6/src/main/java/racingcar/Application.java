9305b7a55cedbddce40ee12ac4dbb3cf6dd40a0d
hyungin0505 <hyungin0505@naver.com>
2025-04-03 15:12:56 +0900

    Week1

--- 고형인/java-racingcar-6/src/main/java/racingcar/Application.java
+++ 고형인/java-racingcar-6/src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 입력 받기
+        Input carNameInput = new CarNameInput();
+        Input countInput = new CountInput();
+
+        // 자동차 이름 쌩자 저장
+        String rawCarList = carNameInput.getInput();
+        // 횟수 입력 받아서 time에 저장
+        int time = Integer.parseInt(countInput.getInput());
+
+        // 레이싱 가보쟈
+        Race race = new Race(rawCarList);
+        race.start(time);
+
+        // 우스앚
+        race.winner();
     }
 }
