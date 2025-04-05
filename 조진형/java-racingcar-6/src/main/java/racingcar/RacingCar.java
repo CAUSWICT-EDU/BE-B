e438b0d751e2d77d0b989c68cca2a7aa3e8c0b68
ErrCode0987 <44331678+macqueen0987@users.noreply.github.com>
2025-04-05 14:49:08 +0900

    Update RacingCar.java
    잘못 올렸었습니다

--- 조진형/java-racingcar-6/src/main/java/racingcar/RacingCar.java
+++ 조진형/java-racingcar-6/src/main/java/racingcar/RacingCar.java
@@ -1,46 +1,26 @@
 package racingcar;
 
-public class Race {
-    private RacingCar[] cars;
+import java.util.Random;
 
-    public void setCars(String[] carNames) {
-        this.cars = new RacingCar[carNames.length];
-        for (int i = 0; i < carNames.length; i++) {
-            this.cars[i] = new RacingCar(carNames[i]);
+public class RacingCar {
+    private String carname;
+    private int position = 0;
+    public RacingCar(String name) {
+        this.carname = name;
+    }
+
+    public void race() {
+        Random rand = new Random();
+        if (rand.nextInt(10) >= 4) {
+            this.position++;
         }
     }
 
-    public void doRace(){
-        for (RacingCar car : cars) {
-            car.race();
-        }
+    public String getName() {
+        return this.carname;
     }
 
-    public void printResult() {
-        for (RacingCar car : cars) {
-            System.out.print(car.getName() + ": ");
-            for (int i = 0; i < car.getPosition(); i++) {
-                System.out.print("-");
-            }
-            System.out.println();
-        }
-        System.out.println();
+    public int getPosition() {
+        return this.position;
     }
-
-    public void getWinner() {
-        int maxPosition = 0;
-        for (RacingCar car : cars) {
-            if (car.getPosition() > maxPosition) {
-                maxPosition = car.getPosition();
-            }
-        }
-        System.out.print("우승자는 ");
-        for (RacingCar car : cars) {
-            if (car.getPosition() == maxPosition) {
-                System.out.print(car.getName() + " ");
-            }
-        }
-        System.out.println("입니다.");
-    }
-
 }
