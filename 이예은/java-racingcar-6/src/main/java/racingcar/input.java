3c76fe2dbe08346d37b65698686ff02d856fcba9
YE <dldpdms0131@gmail.com>
2025-04-06 23:14:56 +0900

    retry

--- 이예은/java-racingcar-6/src/main/java/racingcar/input.java
+++ 이예은/java-racingcar-6/src/main/java/racingcar/input.java
@@ -7,17 +7,27 @@ public class input {
     private ArrayList<Car> carlist = new ArrayList<>();
     private int trynum;
 
+    public input() {}
     Car car = new Car();
     CarMove move = new CarMove();
 
     public void inputValue() {
-        String[] namelist = Console.readLine().split(",");
-        for(String j : namelist)
+        String[] namelist = null;
+        try {
+            namelist = Console.readLine().split(",");
+            if (namelist.length <5) {
+                throw()
+            }
+        } catch (NumberFormatException e) {
+        }
+        for (String j : namelist)
             carlist.add(new Car(j));
     }
 
     public void inputtry() {
         trynum = Integer.parseInt(Console.readLine());
+        if (trynum<0)
+            throw new IllegalArgumentException();
     }
 
     public ArrayList<Car> getcarlist() {
