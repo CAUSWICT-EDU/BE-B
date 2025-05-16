fffd129f7d5b40c15907e0c02bd54112555de83d
hyungin0505 <hyungin0505@naver.com>
2025-05-16 12:06:23 +0900

    Edit: notifySetting에서 enums 사용

---
+++ 고형인/chapter5/src/main/java/cau/ict/btrack/domain/enums/setNotify.java
@@ -0,0 +1,5 @@
+package cau.ict.btrack.domain.enums;
+
+public enum setNotify {
+    ON, OFF
+}
---
+++ 고형인/chapter5/src/main/java/cau/ict/btrack/domain/notifySetting.java
@@ -0,0 +1,27 @@
+package cau.ict.btrack.domain;
+
+import cau.ict.btrack.domain.enums.setNotify;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class notifySetting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private setNotify setAnnouncement;
+
+    @Enumerated(EnumType.STRING)
+    private setNotify setMarketing;
+
+    @Enumerated(EnumType.STRING)
+    private setNotify setBookReturn;
+
+}
