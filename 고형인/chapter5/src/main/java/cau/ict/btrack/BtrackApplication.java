ed94123f28267e3ce693843879b6a5567d7be67d
hyungin0505 <hyungin0505@naver.com>
2025-05-16 12:10:53 +0900

    Edit: Spring Application 자체가 JpaAuditing 사용 가능하도록 어노테이션 추가

--- 고형인/chapter5/src/main/java/cau/ict/btrack/BtrackApplication.java
+++ 고형인/chapter5/src/main/java/cau/ict/btrack/BtrackApplication.java
@@ -2,8 +2,11 @@ package cau.ict.btrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BtrackApplication {
 
 	public static void main(String[] args) {
