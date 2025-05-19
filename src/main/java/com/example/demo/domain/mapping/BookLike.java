64596b883f6d05af8ffe0ff34463b52a64753425
ErrCode0987 <44331678+macqueen0987@users.noreply.github.com>
2025-05-19 15:22:31 +0900

    매핑 테이블 구현
    각각 책의 해시태그, 좋아요, 대출, 사용자 차단 정보를 보유

--- src/main/java/com/example/demo/domain/mapping/BookLike.java
+++ src/main/java/com/example/demo/domain/mapping/BookLike.java
@@ -1,21 +1,22 @@
 package com.example.demo.domain.mapping;
 
+import com.example.demo.domain.Book;
 import com.example.demo.domain.Member;
 import jakarta.persistence.*;
 import com.example.demo.domain.common.BaseTimeEntity;
 
 @Entity
-public class Block extends BaseTimeEntity {
+public class BookLike extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "owner_id")
-    private Member owner;
+    @JoinColumn(name = "book_id")
+    private Book book;
 
     @ManyToOne
-    @JoinColumn(name = "target_id")
-    private Member target;
+    @JoinColumn(name = "member_id")
+    private Member member;
 }
